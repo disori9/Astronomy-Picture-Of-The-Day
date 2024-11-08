c7f2c095dc2781766ab87f645dd2c67d950b2346
Ori <disori777@gmail.com>
2024-11-08 11:55:17 +0800

    Forgot to commit, but basically get api key, url, convert that to json content, then layout the web app.

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import requests
+
+api_key = 'QvJALTXQrz4DLZwFCwLNRpEhAhSi1k67pz2GuvoU'
+url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
+response = requests.get(url)
+
+apod_content = response.json()
+
+st.title(apod_content['title'])
+st.image(apod_content['url'])
+st.write(apod_content['explanation'])
+
